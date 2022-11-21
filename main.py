e5e46b6b8bedc9e61c6f821b48aafe9a32241a1a
Ivanova Polina <Po_ivanova@bk.ru>
2022-11-21 18:58:41 +0300

    дораб

---
+++ main.py
@@ -0,0 +1,33 @@
+import sys
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5 import uic
+
+
+class Circles(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+        self.pushButton.clicked.connect(self.paint)
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_flag(qp)
+            qp.end()
+
+    def paint(self):
+        self.do_paint = True
+        self.repaint()
+
+    def draw_flag(self, qp):
+        qp.setBrush(QColor(255, 255, 0))
+        qp.drawEllipse()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Circles()
+    ex.show()
+    sys.exit(app.exec())
